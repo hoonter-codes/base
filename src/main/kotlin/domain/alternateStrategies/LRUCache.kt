f0243bf05b4e3e6c6895ce1a4881622056348772
hoonter-codes <dhake.baba.met17@gmail.com>
2026-09-13 10:24:47 +0530

    modifications to LRU cache

--- src/main/kotlin/domain/alternateStrategies/LRUCache.kt
+++ src/main/kotlin/domain/alternateStrategies/LRUCache.kt
@@ -1,27 +1,34 @@
 package com.example.domain.alternateStrategies
 
+
 class LRUCache<K, V>(
     private val capacity: Int
-) : LinkedHashMap<K, V>(capacity, 0.75f, true) {
+) {
 
-    // accessOrder = true means even get() mutates the internal access list,
+    // Completely encapsulated inside the class
+    private val internalMap = object : LinkedHashMap<K, V>(capacity, 0.75f, true) {
+        override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
+            return this.size > capacity
+        }
+    }
+
+    // accessOrder = true means even get() mutates the internal access list as it should in a LRU cache,
     // so reads need the lock just like writes.
     @Synchronized
-    override fun get(key: K): V? = super.get(key)
+    fun get(key: K): V? = internalMap[key]
 
     @Synchronized
-    override fun put(key: K, value: V): V? = super.put(key, value)
-
-    @Synchronized
-    override fun putAll(from: Map<out K, V>) = super.putAll(from)
-
-    @Synchronized
-    override fun remove(key: K): V? = super.remove(key)
-
-    @Synchronized
-    override fun clear() = super.clear()
-
-    override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
-        return size > capacity
+    fun put(key: K, value: V) {
+        internalMap[key] = value
     }
+
+    @Synchronized
+    fun remove(key: K): V? = internalMap.remove(key)
+
+    @Synchronized
+    fun clear() = internalMap.clear()
+
+    // Ensure state inspectors are also synchronized!
+    val size: Int
+        @Synchronized get() = internalMap.size
 }
